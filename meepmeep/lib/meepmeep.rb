65e5274819d6e199e224108f6fe113726246b76e
Jason Whittle <jason.whittle@fynanz.com>
2011-06-10 02:35:35 -0400

    Add meepmeep solution.

---
+++ meepmeep/lib/meepmeep.rb
@@ -0,0 +1,6 @@
+# Solution to Facebook engineering puzzle meepmeep
+# http://www.facebook.com/careers/puzzles.php?puzzle_id=2
+# Copyright (C) 2011 Jason Whittle <jason.whittle@gmail.com>
+# Licensed under the terms of the GNU GPLv3
+
+puts 'Meep meep!'
